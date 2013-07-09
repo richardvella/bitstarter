7973367eeca283197bcf5ab8b15504e12e24e1fb
Richard Vella <richard.vella@gmail.com>
2013-07-10 00:05:52 +0200

    Read index.html from disk and use that data in place of Hello World2

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   buffer = fs.readFileSync('index.html');
-  response.send(buffer);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
