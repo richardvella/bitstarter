5097fd8482fd24464acf9ffb6e08c3bbdea73500
Richard Vella <richard.vella@gmail.com>
2013-08-06 21:33:49 +0200

    Edited index.html

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,6 +56,25 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile) {
+
+    console.log(url);
+    
+    rest.get(url).on('complete', function(result, response) 
+{
+	    //if (result instanceof Error) {
+	    //    process.exit(1);
+	    //} else {
+	        var downfile = 'temp.html';
+	        fs.writeFileSync(downfile, result.toString());
+	        var checkJson = checkHtmlFile(downfile, checksfile);
+                var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+	    //}
+    });
+
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +85,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-u, --url <url>', 'Url path').parse(process.argv);
+    
+    var checkFn = 'xzc'; 
+console.log(checkFn);
+    if(program.url) {
+	console.log(program.url);
+        checkFn = checkUrl(program.url, program.checks);
+    } else {
+	checkFn = checkHtmlFile(program.file, program.checks);
+    }
+
+// console.log(checkFn);
+  //  var outJson = JSON.stringify(checkFn, null, 4);
+//    console.log(outJson);
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
